ed4e4756be8316f370b12cb8cc5d4716a145972f
Tameemalam1 <40432937+Tameemalam1@users.noreply.github.com>
2024-07-09 15:22:15 +0530

    Add files via upload
    React Test setup

---
+++ react_test/src/App.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { useEffect , useState} from 'react';
+import './App.css';
+import MovieCard from './MovieCard';
+import SearchIcon from './search.svg';
+
+const API_URL = 'https://www.omdbapi.com?apikey=588098a8';
+const API_local = 'http://localhost:3006';
+
+const App = () => {
+	const [movies ,setMovies] = useState([]);
+	const [searchTerm , setSearchTerm] = useState('');
+	const searchMovies = async (title) => {
+		const response = await fetch(`${API_URL}&s=${title}`);
+		const local_response = await fetch(`${API_local}/rating`)
+		const data = await response.json();
+		const rating_data = await local_response.json();
+
+		console.log(rating_data);
+
+		setMovies(data.Search);
+	}
+
+	useEffect(() => {
+		searchMovies('Spiderman');
+	}, []);
+	return (
+			<div className = "app">
+				<h1>Movie Land</h1>
+
+				<div className = "search">
+					<input
+						placeholder="Search for movies"
+						value={searchTerm}
+						onChange={(e) => setSearchTerm(e.target.value)}
+					/>
+					<img 
+						src={SearchIcon}
+						alt="search"
+						onClick={() => searchMovies(searchTerm)}
+					/>
+				</div>
+
+				{
+					movies.length > 0
+					? (
+						<div className="container">
+							{movies.map((movie) => (
+								< MovieCard movie ={movie} />
+							))}
+						</div>
+					) : (
+						<div className="empty">
+							<h2>No movies found</h2>
+						</div>
+					)
+				}
+
+			</div>
+	);
+}
+
+export default App;
---
+++ react_test/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import App from './App';
+
+// 588098a8
+
+ReactDom.render(<App />, document.getElementById('root'));
